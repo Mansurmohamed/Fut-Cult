17abbf890747016f899c42189903b6202657126c
Mansurmohamed <abdullamansur3@gmail.com>
2023-06-13 23:02:40 -0400

    Redesigned login/logout button

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,11 +1,31 @@
-  import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from '@auth0/auth0-react';
+import styled from 'styled-components';
+
+const SignInButton = styled.button`
+
+font-size: 50px;
+cursor: pointer;
+background-color: black;
+color: white;
+margin: auto;
+border: solid;
+letter-spacing: 1px;
+`;
+
+const PageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
   const LoginButton = () => {
     const { loginWithRedirect, isAuthenticated, user } = useAuth0();
     console.log(isAuthenticated, user)
    
 
-    return !isAuthenticated && <button onClick={loginWithRedirect}>Sign In</button>;
+    return <PageContainer>!isAuthenticated && <SignInButton onClick={loginWithRedirect}>Sign In</SignInButton>
+    </PageContainer>;
   };
 
   export default LoginButton;
